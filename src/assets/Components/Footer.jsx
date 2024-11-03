d37acb84bb12e1548dda1bcaa98b8b8140e32013
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-11-04 00:15:24 +0530

    footer done

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -1,26 +1,25 @@
 const Footer = () => {
   return (
     <div>
-      <footer className="footer bg-neutral text-neutral-content p-10">
+      <footer className="footer bg-base-200 justify-around opacity-80 text-black p-10 border-t-2 ">
         <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <h6 className="footer-title opacity-100">Services</h6>
+          <a className="link opacity-80 link-hover"> Product Support</a>
+          <a className="link opacity-80 link-hover">Order Tracking</a>
+          <a className="link opacity-80 link-hover">Shipping & Delivery</a>
+          <a className="link opacity-80 link-hover">Returns</a>
         </nav>
         <nav>
-          <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <h6 className="footer-title opacity-100">Company</h6>
+          <a className="link opacity-80 link-hover">About Us</a>
+          <a className="link opacity-80 link-hover">Careers</a>
+          <a className="link opacity-80 link-hover">Contact</a>
         </nav>
         <nav>
-          <h6 className="footer-title">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <h6 className="footer-title opacity-100">Legal</h6>
+          <a className="link opacity-80  link-hover">Terms of use</a>
+          <a className="link opacity-80 link-hover">Privacy policy</a>
+          <a className="link opacity-80 link-hover">Cookie policy</a>
         </nav>
       </footer>
     </div>
