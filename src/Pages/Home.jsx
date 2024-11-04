ed944fe942b95eb91df05de3193db4a1b8b1c479
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-11-04 13:33:10 +0530

    card desigine is ready

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import DynamicComponents from "../assets/Components/DynamicComponents";
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-base-100">
       {/* banner with btn & hero img */}
       <Banner
         title={"Upgrade Your Tech Accessorize with Gadget Heaven Accessories"}
--- src/assets/Components/DynamicComponents.jsx
+++ src/assets/Components/DynamicComponents.jsx
@@ -14,7 +14,29 @@ const DynamicComponents = () => {
           <button className="btn btn-wide rounded-full">Macbook</button>
           <button className="btn btn-wide rounded-full">Iphone</button>
         </div>
-        <div className="card-items col-span-9 border-2  border-blue-400"></div>
+        <div className="card-items col-span-9 border-2 w-full p-2 gap-3 grid sm:grid-cols-2 md:grid-cols-3 grid-cols-1 border-blue-400">
+          {/* card  */}
+          <div className="bg-base-100 h-fit rounded-xl hover:scale-105 shadow-lg">
+            <figure className="pt-5 px-5">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="flex flex-col px-6 pt-3 gap-1 pb-5 rounded-xl items-start ">
+              <h2 className="card-title font-bold">Shoes!</h2>
+              <p className="opacity-80">
+                price : <span>$100</span>
+              </p>
+              <div className="">
+                <button className="btn bg-transparent hover:bg-[#9538e2bb] border-1 border-[#9538e267] rounded-full mt-1 px-5 py-2 min-h-0 h-auto">
+                  View Details
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
